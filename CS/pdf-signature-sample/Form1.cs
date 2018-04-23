6449534b1c23e9e2f2e52beaf966f7888f031e47
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/pdf-signature-sample/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+// ...
+
+namespace pdf_signature_sample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            XtraReport1 report = new XtraReport1();
+
+            // Create a new X509Certificate2 object.
+            X509Certificate2 certificate = new X509Certificate2();
+
+            // Initialize a local certificate storage and define its options.
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+            // Initialize a certificate collection and adjust its work.
+            X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
+            X509Certificate2Collection fcollection =
+                (X509Certificate2Collection)collection.Find(X509FindType.FindByTimeValid, DateTime.Now, true);
+            X509Certificate2Collection scollection =
+                X509Certificate2UI.SelectFromCollection(fcollection, "Test Certificate Select",
+                    "Select a certificate from the following list to get information on that certificate",
+                    X509SelectionFlag.SingleSelection);
+            if (scollection.Count > 0)
+                certificate = scollection[0];
+
+            // Define the remaining PDF signature options.
+            report.ExportOptions.Pdf.SignatureOptions.ContactInfo = "contact info";
+            report.ExportOptions.Pdf.SignatureOptions.Location = "location";
+            report.ExportOptions.Pdf.SignatureOptions.Reason = "reason";
+
+            // Assign the created certificate to the signature options of the PDF export options.
+            report.ExportOptions.Pdf.SignatureOptions.Certificate = certificate;
+
+            // Export the report to a PDF file
+            // created in the same folder where the application's .exe file is located.
+            report.ExportToPdf("test.pdf");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the digital signature options when exporting a report to PDF
+
+
+<p>This sample details the implementation of a digital signature for reports exported to PDF via the <strong>ExportOptions.Pdf.SignatureOptions</strong> property of an XtraReport.</p><p>In particular, it illustrates how you can register a local storage of digital certificates that provide a public key to encrypt files.</p><p>For more information, see the following topic in MSDN: <a href="http://msdn.microsoft.com/en-us/library/system.security.cryptography.x509certificates.x509certificate2.aspx"><u>X509Certificate2 Class</u></a>.</p>
+
+<br/>
+
+
